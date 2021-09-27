eef2f8ac073c81abccbee08a1dacb1866e7c11d2
Irfan Asif <hashtag.irfan@gmail.com>
2021-09-27 10:19:53 +0530

    functions

--- function.java
+++ function.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class function {
+    public static int fact(int x) {
+        int rv = 1;
+        for (int i = 1; i <= x; i++) {
+            rv = rv * i;
+        }
+        return rv;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int r = scn.nextInt();
+
+        int nfact = fact(n);
+        int nmrfact = fact(n - r);
+
+        int npr = nfact / nmrfact;
+
+        System.out.println(n + "p" + r + " = " + npr);
     }
 }
