5a2d7b0958b986fcbbd377ef03e013344152c7c8
Irfan Asif <hashtag.irfan@gmail.com>
2021-09-27 10:05:25 +0530

    functions

---
+++ Pattern1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Pattern1 {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int num = scn.nextInt();
+
+    for (int n = 0; n < num; n++) {
+      for (int j = 1; j <= n; j++) {
+        System.out.print("\t");
+      }
+      for (int i = 1; i <= num-n; i++) {
+        System.out.print("*\t");
+      }
+      System.out.println();
+    }
+  }
+}
---
+++ Prime_range.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Prime_range {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int low = scn.nextInt();
+        int high = scn.nextInt();
+        int t = scn.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = scn.nextInt();
+
+
+        }
+
+    }
+}
