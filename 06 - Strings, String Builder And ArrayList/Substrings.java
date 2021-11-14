9962c19da9c34c0d52f7883d4c9d16f47dc1c38b
Irfan Asif <hashtag.irfan@gmail.com>
2021-11-15 01:01:01 +0530

    java-book

--- 06 - Strings, String Builder And ArrayList/Substrings.java
+++ 06 - Strings, String Builder And ArrayList/Substrings.java
@@ -11,4 +11,17 @@ class Empty {
 
 class rectangel {
     int length, width;
+
+    void getDAta(int x, int y) {
+        length = x;
+        width = y;
+    }
+
+    int rectArea() {
+        int area = length * width;
+        return (area);
+    }
 }
+
+// a class with only data fields has no life. The objects created by such a
+// class cannot respond to any messages.
